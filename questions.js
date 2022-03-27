e70a43855739a3ae31cb510296c7746a72a8d4d7
HemalathaT2 <102234389+HemalathaT2@users.noreply.github.com>
2022-03-27 15:38:05 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,59 @@
+/ creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is an _________ language.",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A and B",
+    options: [
+      "getElementbyId()",
+      "getElementByClassName()",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Upon encountering empty statements,what does the Javascript Interpreter do?",
+    answer: "Ignores the statements",
+    options: [
+      "Throws an error ",
+      "Ignores the statements",
+      "Gives a warning",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following methods can be used to display data in some form using Javascript?",
+    answer: "All of the above",
+    options: [
+      "document.write()",
+      "console.log()",
+      "window.alert()",
+      "All of the above"
+    ]
+  },
+ 
+];
